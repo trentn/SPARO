bb169121b0b221572b6549c297b65e64b30738d8
Trent Novelly <snovelly@andrew.cmu.edu>
2019-04-01 01:15:33 +0000

    vision code fix, robot_localization package installed

--- ROS_modules/src/system_state/src/vision_system.cpp
+++ ROS_modules/src/system_state/src/vision_system.cpp
@@ -15,7 +15,7 @@ using namespace cv;
 bool detect_target(system_state::DetectTarget::Request &req,
                    system_state::DetectTarget::Response &res);
 
-enum TARGET {VALVE1, VALVE2, VALVE3, BREAKER};
+enum TargetType {VALVE1, VALVE2, VALVE3, BREAKER};
 
 struct HSV {
     int low_H;
@@ -47,6 +47,23 @@ int main(int argc, char** argv) {
 bool detect_target(system_state::DetectTarget::Request &req,
                    system_state::DetectTarget::Response &res){
     
+    switch(req.target){
+        case VALVE1:  target_color = &blue;
+                             break;
+
+        case VALVE2:  target_color = &orange;
+                             break;
+
+        case VALVE3:  target_color = &blue;
+                             break;
+
+        case BREAKER: target_color = &orange;
+                             break;
+
+        default: break;
+    }
+    
+    
     rs2::colorizer color_map;
     rs2::pipeline pipe;
     rs2::config cfg;
@@ -65,6 +82,7 @@ bool detect_target(system_state::DetectTarget::Request &req,
     {
         frames = pipe.wait_for_frames();
     }
+    // align frames and extract data
     rs2::align align = rs2::align(RS2_STREAM_COLOR);
     auto aligned_frames = align.process(frames);
 
@@ -83,15 +101,11 @@ bool detect_target(system_state::DetectTarget::Request &req,
             Scalar(target_color->low_H, target_color->low_S, target_color->low_V),
             Scalar(target_color->high_H, target_color->high_S, target_color->high_V), color_filtered);
 
-    //mask edges
+    //mask off outside edges
     Mat mask = Mat::zeros(Size(640,480), CV_8U);
     mask(Rect(210,50,210,380)) = 255;
     Mat color_masked;
     bitwise_and(color_filtered, color_filtered.clone(), color_masked, mask);
-
-    //Mat depth_filtered;
-    //bitwise_and(depth, depth.clone(), depth_filtered, color_filtered);
-
 
     //get center of "blob"
     Moments m = moments(color_masked, true);
@@ -100,12 +114,8 @@ bool detect_target(system_state::DetectTarget::Request &req,
     circle(color, p, 5, Scalar(128,0,0), -1);
 
     //extract x,y,z
-    //cout << depth.at<float>((int)p.x, (int)p.y) << endl;    
-    //cout << depth_frame.as<rs2::depth_frame>().get_distance((int)p.x,  (int)p.y) << endl;
-
     float threeD_point[3] = {0};
     float pixel[2] = {p.x, p.y};
-    //default depth scale is 1mm
     rs2_deproject_pixel_to_point(threeD_point, &c_intrinsics, pixel, depth_frame.as<rs2::depth_frame>().get_distance((int)p.x,  (int)p.y));
 
     //bounding boxes
