accb3f8523f217870f4df41a850fbc6be0a52a1e
711suyash <47485841+711suyash@users.noreply.github.com>
2019-04-16 18:24:27 -0400

    Add files via upload

---
+++ ROS_modules/src/system_state/scripts/stepper.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as GPIO
+import time
+ 
+GPIO.setmode(GPIO.BCM)
+ 
+coil_A_1_pin = 18
+coil_A_2_pin = 23
+coil_B_1_pin = 24
+coil_B_2_pin = 17
+ 
+GPIO.setup(coil_A_1_pin, GPIO.OUT)
+GPIO.setup(coil_A_2_pin, GPIO.OUT)
+GPIO.setup(coil_B_1_pin, GPIO.OUT)
+GPIO.setup(coil_B_2_pin, GPIO.OUT)
+
+forward_seq = ['1010', '0110', '0101', '1001']
+reverse_seq = list(forward_seq) # to copy the list
+reverse_seq.reverse()
+ 
+def forward(delay, steps):  
+  for i in range(steps):
+    for step in forward_seq:
+      set_step(step)
+      time.sleep(delay)
+ 
+def backwards(delay, steps):  
+  for i in range(steps):
+    for step in reverse_seq:
+      set_step(step)
+      time.sleep(delay)
+ 
+  
+def set_step(step):
+  GPIO.output(coil_A_1_pin, step[0] == '1')
+  GPIO.output(coil_A_2_pin, step[1] == '1')
+  GPIO.output(coil_B_1_pin, step[2] == '1')
+  GPIO.output(coil_B_2_pin, step[3] == '1')
+ 
+# while True:
+#   set_step('0000')
+#   delay = raw_input("Delay between steps (milliseconds)?")
+#   steps = raw_input("How many steps forward? ")
+#   forward(int(delay) / 1000.0, int(steps))
+#   set_step('0000')
+#   steps = raw_input("How many steps backwards? ")
+#   backwards(int(delay) / 1000.0, int(steps))
+
+while True:
+    set_step('0000')
+    question = input("forward or backward? enter F or B")
+    if question == 'F':
+        degrees = input('enter degrees ')
+        forward(10, int(degrees/1.8))
+    else:
+        degrees = input('enter degrees ')
+        backwards(10,int(degrees/1.8))
+
